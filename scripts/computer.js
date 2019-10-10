a2a17cbaca14bfa8d7dcce87472dfa7f506c2eb9
AntoineBaudot <baudotab@gmail.com>
2019-10-10 02:08:56 +0200

    bug menu2

--- scripts/computer.js
+++ scripts/computer.js
@@ -6,7 +6,7 @@ document.getElementById('tele').addEventListener("mouseout", function () {
 }, false);
 
 */
-
+/*
 let images = new Array()
 function preload() {
     for (i = 0; i < preload.arguments.length; i++) {
@@ -15,15 +15,15 @@ function preload() {
     }
 }
 preload(
-    "../Matrix.fr/src/salle-tv-accueil.jpg",
-    "../Matrix.fr/src/salle-tv-éteint.jpg",
-    "../Matrix.fr/src/salle-tv-poster.jpg",
-    "../Matrix.fr/src/salle-tv-video.jpg",
-    "../Matrix.fr/src/salle-tv-making-of.jpg",
-    "../Matrix.fr/src/salle-tv-about.jpg",
-    "../Matrix.fr/src/salle-tv-galerie.jpg",
+    "../src/salle-tv-accueil.jpg",
+    "../src/salle-tv-éteint.jpg",
+    "../src/salle-tv-poster.jpg",
+    "../src/salle-tv-video.jpg",
+    "../src/salle-tv-making-of.jpg",
+    "../src/salle-tv-about.jpg",
+    "../src/salle-tv-galerie.jpg",
 )
-
+*/
 
 
 let tv1 = document.getElementById("tv1")
@@ -31,64 +31,64 @@ let tele = document.getElementById("tele")
 
 
 tv1.addEventListener("mouseover", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-accueil.jpg)";
+    tele.style.background = "url(../src/salle-tv-accueil.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv1.addEventListener("mouseout", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-éteint.jpg)";
+    tele.style.background = "url(../src/salle-tv-éteint.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 
 tv2.addEventListener("mouseover", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-poster.jpg)";
+    tele.style.background = "url(../src/salle-tv-poster.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv2.addEventListener("mouseout", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-éteint.jpg)";
+    tele.style.background = "url(../src/salle-tv-éteint.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 
 tv3.addEventListener("mouseover", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-video.jpg)";
+    tele.style.background = "url(../src/salle-tv-video.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv3.addEventListener("mouseout", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-éteint.jpg)";
+    tele.style.background = "url(../src/salle-tv-éteint.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv4.addEventListener("mouseover", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-about.jpg)";
+    tele.style.background = "url(../src/salle-tv-about.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv4.addEventListener("mouseout", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-éteint.jpg)";
+    tele.style.background = "url(../src/salle-tv-éteint.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv5.addEventListener("mouseover", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-making-of.jpg)";
+    tele.style.background = "url(../src/salle-tv-making-of.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv5.addEventListener("mouseout", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-éteint.jpg)";
+    tele.style.background = "url(../src/salle-tv-éteint.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv6.addEventListener("mouseover", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-galerie.jpg)";
+    tele.style.background = "url(../src/salle-tv-galerie.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
 tv6.addEventListener("mouseout", function (event) {
-    tele.style.background = "url(../Matrix.fr/src/salle-tv-éteint.jpg)";
+    tele.style.background = "url(../src/salle-tv-éteint.jpg)";
     tele.style.backgroundSize = "cover";
     tele.style.backgroundRepeat = "no-repeat";
 })
